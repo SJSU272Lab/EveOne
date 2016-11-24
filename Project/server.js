4f2cb83b83b35d7929dad95f8dfe768bcc8950d6
Rishindra Reddy <rishindrareddy@hotmail.com>
2016-11-24 12:16:49 -0800

    Implementing Oauth2
    New application in stormpath created.
    api keys and link to the application included in server.js
    
    check readme for updated URI's for authentication.

--- Project/server.js
+++ Project/server.js
@@ -4,10 +4,41 @@ var mongojs = require('mongojs');
 var db = mongojs('user:user@ds149437.mlab.com:49437/eveone', ['studentlist']);
 var bodyParser = require('body-parser');
 
+var logger = require('morgan');
+app.use(logger("dev"));
+
+
+
+var stormpath = require('express-stormpath');
+
+
+app.use(stormpath.init(app,{
+  
+    client: {
+        apiKey: {
+            id: '3A6ERPYU38GCYC8BL0KT6RZ6C',
+            secret:'5rLj+wlNcTrsdahJMMOZ7LHrkzQsEWbq7oplTHFS8u0'
+        }
+    },
+     application:{
+
+    href:'https://api.stormpath.com/v1/applications/6t5AvCQz6ffrIcm9cZk8ds'
+ }
+
+
+}));
+
+
+
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
 
-app.get('/studentlist', function (req, res) {
+
+app.get('/test',stormpath.apiAuthenticationRequired, function(req,res){
+  res.json({ Autheticated: true});
+});
+
+app.get('/studentlist',stormpath.apiAuthenticationRequired, function (req, res) {
   console.log('I received a GET request');
 
   db.studentlist.find(function (err, docs) {
@@ -16,14 +47,14 @@ app.get('/studentlist', function (req, res) {
   });
 });
 
-app.post('/studentlist', function (req, res) {
+app.post('/studentlist',stormpath.apiAuthenticationRequired, function (req, res) {
   console.log(req.body);
   db.studentlist.insert(req.body, function(err, doc) {
     res.json(doc);
   });
 });
 
-app.delete('/studentlist/:id', function (req, res) {
+app.delete('/studentlist/:id',stormpath.apiAuthenticationRequired, function (req, res) {
   var id = req.params.id;
   console.log(id);
   db.studentlist.remove({_id: mongojs.ObjectId(id)}, function (err, doc) {
@@ -31,7 +62,7 @@ app.delete('/studentlist/:id', function (req, res) {
   });
 });
 
-app.get('/studentlist/:id', function (req, res) {
+app.get('/studentlist/:id',stormpath.apiAuthenticationRequired, function (req, res) {
   var id = req.params.id;
   console.log(id);
   db.studentlist.findOne({_id: mongojs.ObjectId(id)}, function (err, doc) {
@@ -39,7 +70,7 @@ app.get('/studentlist/:id', function (req, res) {
   });
 });
 
-app.put('/studentlist/:id', function (req, res) {
+app.put('/studentlist/:id',stormpath.apiAuthenticationRequired, function (req, res) {
   var id = req.params.id;
   console.log(req.body.name);
   db.studentlist.findAndModify({
@@ -51,5 +82,8 @@ app.put('/studentlist/:id', function (req, res) {
   );
 });
 
-app.listen(3000);
-console.log("Server running on port 3000");
+app.on('stormpath.ready', function() {
+  app.listen(3000);
+  console.log("Server running on port 3000");
+});
+
