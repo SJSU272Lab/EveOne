313b633a233cef9bc436504663482358a5d711d5
Akshay Sonvane <akshay.sonvane93@gmail.com>
2016-12-06 18:59:36 -0800

    Updated android app.

--- Project/Android/EveOne/app/src/main/java/com/akshaysonvane/eveone/MainActivity.java
+++ Project/Android/EveOne/app/src/main/java/com/akshaysonvane/eveone/MainActivity.java
@@ -1,6 +1,9 @@
 package com.akshaysonvane.eveone;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.webkit.CookieManager;
@@ -19,6 +22,9 @@ public class MainActivity extends AppCompatActivity
     @BindView(R.id.webview)
     WebView webView;
 
+    @BindView(R.id.swipeToRefresh)
+    SwipeRefreshLayout swipeRefreshLayout;
+
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -30,11 +36,57 @@ public class MainActivity extends AppCompatActivity
         if (getSupportActionBar() != null)
             getSupportActionBar().hide();
 
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorAccent);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener()
+        {
+            @Override
+            public void onRefresh()
+            {
+                webView.loadUrl("javascript:window.location.reload( true )");
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
         CookieManager.getInstance().setAcceptCookie(true);
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
 
         webView.loadUrl(Utils.BASE_URL);
+    }
+
+
+    public void onBackPressed()
+    {
+        if (webView.canGoBack())
+        {
+            webView.goBack();
+        }
+        else
+        {
+            AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+            alertDialogBuilder.setTitle("Exit Application");
+            alertDialogBuilder.setMessage("Are you sure ?").setCancelable(false).setPositiveButton("Yes", new DialogInterface.OnClickListener()
+            {
+                public void onClick(DialogInterface dialog, int id)
+                {
+                    moveTaskToBack(true);
+                    android.os.Process.killProcess(android.os.Process.myPid());
+                    System.exit(1);
+                }
+            })
+
+                    .setNegativeButton("No", new DialogInterface.OnClickListener()
+                    {
+                        public void onClick(DialogInterface dialog, int id)
+                        {
+
+                            dialog.cancel();
+                        }
+                    });
+
+            AlertDialog alertDialog = alertDialogBuilder.create();
+            alertDialog.show();
+        }
     }
 }
