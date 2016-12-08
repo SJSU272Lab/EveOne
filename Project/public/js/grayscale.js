b8a5694aed7565bf0ff83b85fe0551b235c79ef2
MandarGade <mandargade.2997@gmail.com>
2016-12-07 17:05:28 -0800

    updated java script

--- Project/public/js/grayscale.js
+++ Project/public/js/grayscale.js
@@ -37,7 +37,7 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(37.3352, (-121.8811)));
 });
 
 function init() {
@@ -48,12 +48,12 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(37.3352, (-121.8811)), // SJSU
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false,
+        scrollwheel: true,
+        draggable: true,
 
         // How you would like to style the map. 
         // This is where you would paste any style found on Snazzy Maps.
@@ -176,7 +176,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng((37.3352, (-121.8811)));
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
